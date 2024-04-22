5d2903d609a254dba5ce9435ece3517364ef53fe
harshilButaniyudiz <159538734+harshilButaniyudiz@users.noreply.github.com>
2024-04-22 10:36:45 +0530

    Loding Canvas

--- Assets/Script/Ui/Loding_Canvas.cs
+++ Assets/Script/Ui/Loding_Canvas.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Loding_Canvas : MonoBehaviour
+public class Loding_Canvas : BaseScreen
 {
     public Slider LoadingSlider;
+    bool LodingComplete = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,11 @@ public class Loding_Canvas : MonoBehaviour
     void Update()
     {
 
-       // if(LoadingSlider.value == 0)
-       // {
-       //     UiManager.instance.SwitchScreen(GameScreens.Home);
-       // }
-    }  //
+        if(LoadingSlider.value >= 0.9f && !LodingComplete)
+        {
+            Debug.Log("Call");
+            UiManager.instance.SwitchScreen(GameScreens.Home);
+            LodingComplete = true;
+        }
+    }  
 }
